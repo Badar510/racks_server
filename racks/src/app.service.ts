1e2ce7c1fdcdcc1da67f34e8c36b027406e71c15
Badar Waqar <badarwaqar510@gmail.com>
2021-12-14 00:38:18 +0500

    created some new method by calling apis from pi

--- racks/src/app.service.ts
+++ racks/src/app.service.ts
@@ -1,14 +1,92 @@
 import { Injectable } from '@nestjs/common';
 const axios = require('axios');
 const fs = require('fs');
+let timeInterval = 5; // in secon
+const defaultGateway = require('default-gateway');
 
 @Injectable()
 export class AppService {
-  async getStatus(query) {
-    /* for (let i = 2; i <= 200; i++) {
-      console.log(`"${i}":"192.168.1.${i}",`);
-    } */
+  constructor() {
+    this.saveJsonAndCallStagingAPI();
+  }
 
+  async saveJsonAndCallStagingAPI() {
+    let ctr = 2;
+    const { gateway } = await defaultGateway.v4();
+    const gatewayArray = gateway.split('.');
+    let array = [
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'H',
+      'I',
+      'J',
+      'K',
+      'L',
+      'M',
+    ];
+    let jsonStr = '{';
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 1; j <= 8; j++) {
+        jsonStr += `"${array[i]}-0${j}": "${gatewayArray[0]}.${gatewayArray[1]}.${gatewayArray[2]}.${ctr}"`;
+        if (!(i == array.length - 1 && j == 8)) {
+          jsonStr += `,`;
+        }
+        // console.log(
+        //   `"${array[i]}-0${j}": "${gatewayArray[0]}.${gatewayArray[1]}.${gatewayArray[2]}.${ctr}",`,
+        // );
+        ctr++;
+      }
+    }
+    jsonStr += '}';
+
+    await fs.writeFile('./src/device_ips.json', jsonStr, (err) => {
+      if (err) throw err;
+    });
+    this.getDataStagingAPI();
+  }
+
+  async getDataStagingAPI() {
+    async function apiCall() {
+      try {
+        const response = await axios.get(
+          'https://stagingapi.grocery.rideairlift.com/v2/orders/packed/compartments?warehouse=37',
+          {
+            headers: {
+              auth: 'Groc3R@Sm@rtR@ck',
+            },
+          },
+        );
+        const allData = response.data;
+        allData.forEach(async (obj) => {
+          const fileData = fs.readFileSync('./src/device_ips.json', 'utf8');
+          const device_ips = JSON.parse(fileData);
+          const device_ip = device_ips[obj['compartment']];
+
+          const getValuesUrl = `http://${device_ip}/racks?code=${obj['code']}&var1=123`;
+          try {
+            const response = await axios.post(getValuesUrl);
+            return response.data;
+          } catch (error) {
+            console.error(error);
+            return error;
+          }
+        });
+
+        return response.data;
+      } catch (error) {
+        console.error(error);
+        return error;
+      }
+    }
+    setInterval(apiCall, timeInterval * 1000);
+  }
+
+  async getStatus(query) {
     try {
       const fileData = fs.readFileSync('./src/device_ips.json', 'utf8');
       const device_ips = JSON.parse(fileData);
