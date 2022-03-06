34eebb6dd1e3df068fe32bfa68408a77abd0ed6d
Badar <badarwaqar510@gmail.com>
2022-03-07 04:54:27 +0500

    iew7

--- racks/src/app.service.ts
+++ racks/src/app.service.ts
@@ -14,7 +14,7 @@ const foreignApiLimiter = new Bottleneck({
   minTime: 5000,
 });
 const foreignApi: string =
-  'https://api.airliftgrocer.com/v2/orders/packed/compartments?warehouse=502';
+  'https://api.airliftgrocer.com/v2/orders/packed/compartments?warehouse=497';
 
 @Injectable()
 export class AppService {
